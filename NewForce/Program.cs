43a2bf641689e8e73241afd9d1680a08afd23cc1
Fiona Wee <fwee@oswego.edu>
2024-11-03 21:01:32 -0500

    first commit

---
+++ NewForce/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NewForce
+{
+    internal class Program
+    {
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=NewForce;integrated security=true; TrustServerCertificate=True;";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
